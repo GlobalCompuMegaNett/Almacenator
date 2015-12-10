386a95625eeda2703a8b0e61f176a6282c2aaa79
GlobalCompuMegaNett <josemanuelplana.global@gmail.com>
2015-12-10 11:49:25 +0100

    Update - Iteracion 7
    Iteracion 7 Completa

---
+++ src/main/java/iteracion7/dominio/GestorBorrar.java
@@ -0,0 +1,11 @@
+package iteracion7.dominio;
+
+import java.io.IOException;
+
+public class GestorBorrar {
+
+	public static void BorrarCliente(String nombre) throws IOException {
+		ContactoDaoBorrar.BorrarCliente(nombre);
+	}
+
+}
